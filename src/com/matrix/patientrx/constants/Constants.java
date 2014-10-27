a0c4de639190546996666171d468126f924f6ec8
Vishal Vijay <0vishalvijay0@gmail.com>
2014-10-28 01:26:50 +0530

    removed secrets

---
+++ src/com/matrix/patientrx/constants/Constants.java
@@ -0,0 +1,20 @@
+package com.matrix.patientrx.constants;
+
+public class Constants {
+	public static final String AWS_S3_VIDEO_BUCKET = "bucketname";
+	public static final String AWS_S3_ACCESS_KEY_ID = "key_id";
+	public static final String AWS_S3_SECRET_KEY = "key_secret";
+	public static final String USER_NAME = "username";
+	public static final String TOKEN = "token";
+	public static final String GOOGLE_LOGIN = "google_oauth2";
+	public static final String FACEBOOK_LOGIN = "facebook";
+	public static final String SESSION_ID = "sessionid";
+	public static final String EMAIL_ID = "emailid";
+	public static final String USER_ID = "userid";
+	public static final String EXTRA_IMAGE_PATH = "imagepath";
+	public static final String EXTRA_CASE = "case";
+	public static final String EXTRA_CASE_ID = "caseId";
+
+	public static final String EXTRA_IMAGE_URL = null;
+
+}
